8a3af07872b249a00d1111096299c70d01470231
natmali404 <58657829+natmali404@users.noreply.github.com>
2025-03-04 16:52:32 +0100

    begin working on error 525 retry

--- backend/server.js
+++ backend/server.js
@@ -46,30 +46,45 @@ app.get("/", (request, response) => {
 
 const downloadFolder = path.join(path.resolve(), "downloads");
 
+const MAX_RETRIES = 3; //for HTTP 525
+const RETRY_DELAY = 2000;
+
 const getDownloadLink = async (url) => {
+  const headers = {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    Accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    Connection: "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    TE: "Trailers",
+  };
+
   try {
-    const { data } = await axios.get(url, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        Accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.9",
-        Connection: "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-        TE: "Trailers",
-      },
-    });
+    const { data } = await axios.get(url, { headers });
     const $ = cheerio.load(data);
     const epubLink = $("a[href*='.epub']").attr("href");
+
     if (!epubLink) {
       throw new Error(".epub link not found on the page!");
     }
+
     console.log("Download link found: ", epubLink);
     return "https://archiveofourown.org" + epubLink;
   } catch (error) {
-    console.error("Error getting download link: ", error);
+    if (error.response && error.response.status === 525 && retries > 0) {
+      console.log(
+        `HTTP 525 error. Retrying... (${
+          MAX_RETRIES - retries + 1
+        }/${MAX_RETRIES})`
+      );
+      await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
+      return getDownloadLink(url, retries - 1);
+    }
   }
+  console.error("Error getting download link: ", error.message);
+  throw error;
 };
 
 const getFileName = (downloadLink) => {
